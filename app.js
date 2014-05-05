ed4d36adf750cfdf7a620e15f786be1e25c45636
Lalli Nuorteva <lalnuor@gmail.com>
2014-05-05 18:37:00 +0300

    refaktoroitu

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express();
 var id3 = require('id3js');
 var fs = require('fs');
 var async = require('async');
-var songdir = process.env.HOME+"/projects/protify-songs";
+var songFolderPath = process.env.HOME+"/projects/protify-songs";
+var pg = require('pg');
+var conString = "postgres://@localhost/database";
+
+var params = { host: "localhost", user: "lallinuo", password: "e1312e9207f01e4d", database: "lallinuo", ssl: true};
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,16 +16,16 @@ app.all('*', function(req, res, next) {
  });
 
 
+//Palauttaa JSONina kaikkien biisien pathit ja niihin liittyvät tagit
 app.get('/tracks', function(req, res){
-  var filesWithPrefix;
-  var mp3Infos = [];
-
-  fs.readdir(songdir,function(err,files){
-    filesWithPrefix= files.map(function(x){
-      return {path: songdir+"/"+x};
+  //käy läpi biisit halutusta kansiosta ja luo jokaisesta oman olion talettaen pathiin kappaleen polun
+  fs.readdir(songFolderPath,function(err,files){
+    var songs = files.map(function(x){
+      return {path: songFolderPath+"/"+x};
     });
-
-    async.map(filesWithPrefix,function(file,callback){
+    //käy kaikkien biisien pathit läpi, ja lisää niihin tagsit id3 kirjaston avulla
+    //async map, koska id3 on asynkroninen
+    async.map(songs,function(file,callback){
       id3({ file: file.path, type: id3.OPEN_LOCAL }, function(err, tags) {
         file.tags = tags;
         callback(err,file);
@@ -31,6 +35,7 @@ app.get('/tracks', function(req, res){
     });
   });
 });
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
       console.log("Listening on " + port);
