c7b9aa1896dad1eff906409500f7c8a7c03e0dfa
Lalli Nuorteva <lalnuor@gmail.com>
2014-05-05 15:42:00 +0300

    biisien tagit jsonina

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var id3 = require('id3js');
 var fs = require('fs');
+var async = require('async');
+var songdir = process.env.HOME+"/projects/protify-songs";
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,14 +13,24 @@ app.all('*', function(req, res, next) {
 
 
 app.get('/tracks', function(req, res){
+  var filesWithPrefix;
+  var mp3Infos = [];
 
-  //id3({ file: './DucK_Classicalised-Dub-160.mp3', type: id3.OPEN_LOCAL }, function(err, tags) {
-    //  res.send(tags);
-  //});
+  fs.readdir(songdir,function(err,files){
+    filesWithPrefix= files.map(function(x){
+      return {path: songdir+"/"+x};
+    });
 
-  res.send("Hello CORS")
+    async.map(filesWithPrefix,function(file,callback){
+      id3({ file: file.path, type: id3.OPEN_LOCAL }, function(err, tags) {
+        file.tags = tags;
+        callback(err,file);
+      });
+    },function(err,files){
+      res.send(files);
+    });
+  });
 });
-
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
       console.log("Listening on " + port);
