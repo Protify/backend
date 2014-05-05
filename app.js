03d7d5f604ee120c3eff5a981d0f647a8f1aafbe
Lalli Nuorteva <lalnuor@gmail.com>
2014-05-05 13:58:39 +0300

    port fixd

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/', function(req, res){
   var test = {track: "Love Generation", artist: "Bob Sinclar", time: "3:30", album: "Love Generation"}
   res.send(test);
 });
+
 var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+      console.log("Listening on " + port);
+});
 
