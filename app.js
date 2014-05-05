c2a308674cc13076c47e4acf54d051ac0071d2df
Lalli Nuorteva <lalnuor@gmail.com>
2014-05-05 14:00:34 +0300

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/tracks', function(req, res){
   var test = {track: "Love Generation", artist: "Bob Sinclar", time: "3:30", album: "Love Generation"}
   res.send(test);
 });
