ddf7d545fbe7188936a016b1626ce1cd9a8927a6
Lalli Nuorteva <lalnuor@gmail.com>
2014-05-05 14:28:31 +0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var app = express();
+var id3 = require('id3js');
+var fs = require('fs');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowedDomains);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.configure(function() {
+	app.use(allowCrossDomain);
+});
+
 
 app.get('/tracks', function(req, res){
-  var test = {track: "Love Generation", artist: "Bob Sinclar", time: "3:30", album: "Love Generation"}
-  res.send(test);
+
+  //id3({ file: './DucK_Classicalised-Dub-160.mp3', type: id3.OPEN_LOCAL }, function(err, tags) {
+    //  res.send(tags);
+  //});
+
+  res.send("Hello CORS")
 });
 
 var port = Number(process.env.PORT || 5000);
